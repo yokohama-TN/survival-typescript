a687de86f70edd923f098f1a1d6d2c4c2b68de4f
yokohama.t <yokohama.t@impl.co.jp>
2024-07-24 08:32:19 +0900

    feat: eslint学習

---
+++ eslint-tutorial/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {
+        //     "no-console": "error",
+        //     camelcase: ["error", { properties: "never" }],
+        //     semi: ["error", "always"],
+        "import/prefer-default-export": "off",
+        quotes: ["error", "double"],
+    },
+    extends: ["airbnb-base"],
+};
